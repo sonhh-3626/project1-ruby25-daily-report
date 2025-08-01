d94c13e3d602b2c3ecf1c425c413d04688611ead
sonhh-3626 <ha.hong.son@sun-asterisk.com>
2025-08-01 23:43:23 +0700

    [Admin] Thêm/Xóa manager cho 1 bộ phận

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,23 @@
+module UserHelper
+  def role_options_for_select
+    options_for_select(
+      Settings.FILTER_ROLE.map do |role|
+        [I18n.t("users.roles.#{role}"), role.to_sym]
+      end
+    )
+  end
+
+  def department_options_for_select
+    options_from_collection_for_select(
+      Department.all,
+      :id,
+      :name,
+      params[:department_id]
+    )
+  end
+
+  def department_options_for_edit
+    [[t("users.form.no_department"), nil]] +
+      (@departments || []).map{|d| [d.name, d.id]}
+  end
+end
